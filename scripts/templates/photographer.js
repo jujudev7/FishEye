aa1c8c27c0c839acc165ba6f74f19033d4b67adb
Julien <penna.julien@gmail.com>
2024-02-01 22:06:08 +0100

    datas added in template photographer

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,18 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const location = document.createElement( 'h3' );
+        location.textContent = city + ', ' + country;
+        const baseline = document.createElement( 'p' );
+        baseline.textContent = tagline;
+        const pricing = document.createElement( 'p' );
+        pricing.textContent = price + '€/jour';
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(baseline);
+        article.appendChild(pricing);
+        
         return (article);
     }
     return { name, picture, getUserCardDOM }
